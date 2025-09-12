04cc0fcf861f3e2a5425f220cb7460883c6bb61d
Andrew Ostler <os@expert-sleepers.co.uk>
2025-09-12 12:54:49 +0100

    added new airwindows plug-ins

--- airwindows/src/kCathedral5.cpp
+++ airwindows/src/kCathedral5.cpp
@@ -2,8 +2,9 @@
 #include <new>
 #include <distingnt/api.h>
 #define AIRWINDOWS_NAME "kCathedral5"
-#define AIRWINDOWS_DESCRIPTION "kCathedral5"
+#define AIRWINDOWS_DESCRIPTION "Lets you place yourself anywhere you like in the space."
 #define AIRWINDOWS_GUID NT_MULTICHAR( 'A','k','C','e' )
+#define AIRWINDOWS_TAGS kNT_tagEffect | kNT_tagReverb
 enum {
 
 	kParam_A =0,
