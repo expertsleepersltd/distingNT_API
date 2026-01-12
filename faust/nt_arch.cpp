3b3e08cbe0ee33fbc384f747f8d57de55c513cd0
Andrew Ostler <os@expert-sleepers.co.uk>
2026-01-12 09:47:04 +0000

    updated architecture file for current faust 2.83.1

--- faust/nt_arch.cpp
+++ faust/nt_arch.cpp
@@ -85,6 +85,9 @@ struct UI
 struct dsp_memory_manager
 {
 	enum _mem { kMemClass, kMemInstance };
+	
+    enum MemType { kInt32, kInt32_ptr, kFloat, kFloat_ptr, kDouble, kDouble_ptr, kQuad, 
+        kQuad_ptr, kFixedPoint, kFixedPoint_ptr, kObj, kObj_ptr, kSound, kSound_ptr };
 
 	size_t	total;
 	bool 	first;
@@ -98,7 +101,7 @@ struct dsp_memory_manager
 		first = true;
 	}
 	void end() {}
-	void info(size_t size, size_t reads, size_t writes)
+	void info(const char* name, MemType type, size_t count, size_t size, size_t reads, size_t writes)
 	{
 		if ( mode == kMemClass )
 		{
