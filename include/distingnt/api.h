10171845e2088cf77b9fb3787965f544840aa0a1
Andrew Ostler <os@expert-sleepers.co.uk>
2026-09-14 15:51:27 +0100

    use .text.nt_dram for the section name instead of ._nt_dram

--- include/distingnt/api.h
+++ include/distingnt/api.h
@@ -388,7 +388,7 @@ enum { kNT_parameterStringSize = 64 };
 /*
  * Attribute tag to place a function in DRAM.
  */
-#define _NT_DRAM_SECTION __attribute__ ((section ("._nt_dram")))
+#define _NT_DRAM_SECTION __attribute__ ((section (".text.nt_dram")))
 
 /*
  * Structure that defines an algorithm factory.
